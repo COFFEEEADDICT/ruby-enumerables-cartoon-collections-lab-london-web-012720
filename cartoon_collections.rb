d8fee2a0eaa8dae3eaad0930acf927a47a38a2e2
Bryn Heath <brynheath1@gmail.com>
2020-01-25 00:18:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+    arr.each_with_index {|i,x| puts "#{x+1} #{i}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+   arr = arr.collect { |i| i+"!"}
+   arr = arr.map { |c| c.capitalize }
+   return arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+neww = []
+res = ""
+ neww = arr.collect {|i| i.size <= 4}
+ p neww
+ res = neww.any? {|x| x == false}
+ return res
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.include(cheese_types) {|i| i == cheese_types.map}
 end
